b75df1a49983fa46bcbbce433703488af1ee1f35
claudia99 <clauclau9999@gmail.com>
2020-03-19 13:45:22 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <string.h>
+#include <string>
+using namespace std;
+ifstream fin("AFD.in");
+ofstream fout("AFD.out");
+int main()
+{
+    int n;
+    fin>>n;
+    char alf[1000];
+    long int m[20][20][20];
+    int i,j,k,l;
+
+    for(i=0; i<=n; i++)
+        for(j=0; j<=n; j++)
+            for(k=0; k<=n; k++)
+                m[i][j][k]=-1;
+    int nr;
+    char ch;
+
+    // n - indicele maxim de stari ale automatului ie n+1 stari
+
+
+    int fi[n+1];
+    for(i=0; i<=n; i++)
+        fin>>fi[i];
+    fin>>alf;
+    // k - nr de tranzitii
+
+    fin>>k;
+    int start,stop;
+//    for(i=0; i<=n; i++)
+//    {
+//        for(j=0; j<=n; j++)
+//        {
+//
+//            for(l=0; l<=n; l++)
+//                cout<<m[i][j][l]<<" ";
+//            cout<<endl;
+//        }
+//        cout<<endl;
+//    }
+
+    for(i=0; i<k; i++)
+    {
+        int ok=0;
+        fin>>ch;
+        fin>>start>>stop;
+        if(start>n || start<0 || stop>n|| stop<0)
+        {
+            fout<<"EROARE - stare invalida";
+            return 0;
+        }
+        for(l=0; l<=n&&ok==0; l++)
+            if(m[start][stop][l]==-1)
+            {
+                m[start][stop][l]=ch-48;
+                ok=1;
+            }
+
+    }
+//    for(i=0; i<=n; i++)
+//    {
+//        for(j=0; j<=n; j++)
+//        {
+//
+//            for(l=0; l<=n; l++)
+//                cout<<m[i][j][l]<<" ";
+//            cout<<endl;
+//        }
+//        cout<<endl;
+//    }
+    fin>>nr;
+    string cuv[nr];
+    string a;
+    for(i=0; i<nr; i++)
+    {
+        fin>>a;
+        cuv[i]=a;
+    }
+//    for(i=0; i<nr; i++)
+//        fout<<cuv[i]<<endl;
+//
+//    fout<<endl;
+    int conditie;
+    for(j=0; j<nr; j++)
+    {
+        string cuvact=cuv[j];
+        int index=0;
+        for(i=0; i<cuvact.size(); i++)
+        {
+            char l=cuvact[i];
+            conditie=0;
+
+            for(int a=0; a<=n&&conditie==0; a++)
+                for(int b=0; b<=n; b++)
+                {
+                    if(l-m[index][a][b]==48)
+                    {
+                        //cout<<"diferenta "<<l-m[index][a][b]<<endl<<"index "<<index<<endl<<"a "<<a<<endl;
+                        conditie=1;
+                        index=a;
+                        break;}
+
+            }
+            if(conditie==0)
+            {
+                fout<<"NU"<<endl;
+                break;
+            }
+        }
+        if(conditie==1&& fi[index]==1)
+            {fout<<"DA"<<endl; cout<<index<<fi[index];}
+        else
+            if(conditie==1)
+            fout<<"NU"<<endl;
+
+    }
+
+
+
+    fin.close();
+    fout.close();
+    return 0;
+}
